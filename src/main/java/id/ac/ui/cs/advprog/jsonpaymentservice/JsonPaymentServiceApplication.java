e92e12bcace017ea667b044c1c048647775a9a5b
r3gulus-4rcturus <muh.lanangzalkifla@gmail.com>
2026-02-19 16:53:40 +0700

    initial commit

---
+++ src/main/java/id/ac/ui/cs/advprog/jsonpaymentservice/JsonPaymentServiceApplication.java
@@ -0,0 +1,13 @@
+package id.ac.ui.cs.advprog.jsonpaymentservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JsonPaymentServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JsonPaymentServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/id/ac/ui/cs/advprog/jsonpaymentservice/JsonPaymentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package id.ac.ui.cs.advprog.jsonpaymentservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JsonPaymentServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
