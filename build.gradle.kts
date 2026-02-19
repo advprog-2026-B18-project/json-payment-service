6594a7dc8b1b2403a98ccb66c7e94c88e41c5ad8
r3gulus-4rcturus <muh.lanangzalkifla@gmail.com>
2026-02-19 19:29:45 +0700

    add jpa and postgre dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -27,6 +27,8 @@ repositories {
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
     implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    runtimeOnly("org.postgresql:postgresql")
     compileOnly("org.projectlombok:lombok")
     developmentOnly("org.springframework.boot:spring-boot-devtools")
     annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
