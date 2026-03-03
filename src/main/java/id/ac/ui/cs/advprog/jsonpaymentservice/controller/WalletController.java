8c5b26fd1a43436c924889e6d1791d9996d7cab7
r3gulus-4rcturus <muh.lanangzalkifla@gmail.com>
2026-03-03 11:40:40 +0700

    feat: add /wallet/me endpoint  (without jwt validation)

--- src/main/java/id/ac/ui/cs/advprog/jsonpaymentservice/controller/WalletController.java
+++ src/main/java/id/ac/ui/cs/advprog/jsonpaymentservice/controller/WalletController.java
@@ -1,5 +1,6 @@
 package id.ac.ui.cs.advprog.jsonpaymentservice.controller;
 
+import id.ac.ui.cs.advprog.jsonpaymentservice.dto.WalletMinimalResponse;
 import id.ac.ui.cs.advprog.jsonpaymentservice.model.Wallet;
 import id.ac.ui.cs.advprog.jsonpaymentservice.service.WalletService;
 import org.springframework.http.HttpStatus;
@@ -11,13 +12,28 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/v1/wallets")
+@RequestMapping("/api/wallet")
 public class WalletController {
 
     private final WalletService walletService;
 
     public WalletController(WalletService walletService) {
         this.walletService = walletService;
+    }
+
+    // TODO: implement validasi jwt dulu wak
+    // TODO: tambain param buat get jwt nya (?)
+    @GetMapping("/me")
+    public ResponseEntity<WalletMinimalResponse> getCurrentUserWallet() {
+      
+      // TODO: validasi jwt nya, kalo ga valid return 401 unauthorized
+      // TODO: kalo valid, extract user id nya, taro di param getWalletByUserId
+      Wallet wallet = walletService.getWalletByUserId("userId"); 
+
+      long withdrawable_balance = wallet.getBalance() - wallet.getEscrowBalance();
+      WalletMinimalResponse response = new WalletMinimalResponse(wallet.getWalletId(), wallet.getUserId(), withdrawable_balance); 
+      
+      return ResponseEntity.ok(response);
     }
 
     @PostMapping("/{userId}")
