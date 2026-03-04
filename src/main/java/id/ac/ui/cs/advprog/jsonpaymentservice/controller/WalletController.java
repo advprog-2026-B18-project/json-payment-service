46e141821b6f95eb104f960564be14bc543b5c71
r3gulus-4rcturus <muh.lanangzalkifla@gmail.com>
2026-03-04 15:52:57 +0700

    fix: test simple endpoint for deployment

--- src/main/java/id/ac/ui/cs/advprog/jsonpaymentservice/controller/WalletController.java
+++ src/main/java/id/ac/ui/cs/advprog/jsonpaymentservice/controller/WalletController.java
@@ -11,13 +11,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/v1/wallets")
+@RequestMapping("/wallets")
 public class WalletController {
 
     private final WalletService walletService;
 
     public WalletController(WalletService walletService) {
         this.walletService = walletService;
+    }
+
+    @GetMapping("/test")
+    public String test(){
+        return "test";
     }
 
     @PostMapping("/{userId}")
